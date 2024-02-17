7f01d31eaeffe29f1a4d9dcbe3073c8bcacc53ca
Hilal Agil <hilaal@gmail.com>
2024-02-17 09:23:22 +0100

    init commit

---
+++ src/components/header/index.jsx
@@ -0,0 +1,68 @@
+"use client";
+import styles from "./style.module.scss";
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { opacity, background } from "./anim";
+import Nav from "./nav";
+
+export default function index() {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <div className={styles.header}>
+      <div className={styles.bar}>
+        <Link href="/">
+          <Image
+            src="/logo.svg"
+            alt="Tenzro"
+            width={90} // Replace with your desired width
+            height={10} // Replace with your desired height
+            priority // Load the logo quickly
+            className="mt-1"
+          />
+        </Link>
+        <div
+          onClick={() => {
+            setIsActive(!isActive);
+          }}
+          className={styles.el}
+        >
+          <div
+            className={`${styles.burger} ${
+              isActive ? styles.burgerActive : ""
+            }`}
+          ></div>
+          <div className={styles.label}>
+            <motion.p
+              variants={opacity}
+              animate={!isActive ? "open" : "closed"}
+            >
+              Menu
+            </motion.p>
+            <motion.p variants={opacity} animate={isActive ? "open" : "closed"}>
+              Close
+            </motion.p>
+          </div>
+        </div>
+        <motion.div
+          variants={opacity}
+          animate={!isActive ? "open" : "closed"}
+          className={styles.shopContainer}
+        >
+          <p className={styles.shop}>Shop</p>
+          <div className={styles.el}></div>
+        </motion.div>
+      </div>
+      <motion.div
+        variants={background}
+        initial="initial"
+        animate={isActive ? "open" : "closed"}
+        className={styles.background}
+      ></motion.div>
+      <AnimatePresence mode="wait">{isActive && <Nav />}</AnimatePresence>
+    </div>
+  );
+}
