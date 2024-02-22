01afec8ba53b788cab270b81fedc14e31a2b6cd3
Hilal Agil <hilaal@gmail.com>
2024-02-22 14:31:24 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,29 +4,17 @@ import Header from '../components/header';
 import Link from 'next/link';
 import Head from 'next/head';
 
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Tenzro',
+  description: 'Build smarter, safer web3 applications',
+}
+
 export default function RootLayout({ children }) {
   return (
-    <div>
-      <Head>
-        {/* Meta tags for SEO */}
-        <title>Tenzro</title>
-        <meta name="description" content="Build smarter, safer web3 applications" />
-
-        {/* Open Graph tags */}
-        <meta property="og:title" content="Tenzro" />
-        <meta property="og:description" content="Tenzro's novel digital infrastructure injects artificial intelligence and encryption into the blockchain, enabling developers to build web3 applications that are intelligent and secure." />
-        <meta property="og:image" content="https://tenzro.com/og.png" />
-        <meta property="og:url" content="https://tenzro.com/og.png" />
-        <meta property="og:type" content="website" />
-
-        {/* Twitter Card tags */}
-        <meta name="twitter:card" content="Build smarter, safer web3 applications" />
-        <meta name="twitter:title" content="Tenzro" />
-        <meta name="twitter:description" content="Tenzro's digital infrastructure injects artificial intelligence and encryption into the blockchain, enabling developers to build web3 applications that are intelligent and secure." />
-        <meta name="twitter:image" content="https://tenzro.com/twitter-card.png" />
-
-      </Head>
-      <div>
+    <html lang="en">
+      <body className={inter.className}>
         <Header />
         {children}
         <div className='text-neutral-400 md:text-base text-base text-center md:text-right pt-16 pb-24 md:p-12 mono-light mt-12 md:mt-24 w-full md:flex md:justify-between'>
@@ -41,7 +29,7 @@ export default function RootLayout({ children }) {
           </div>
           <p className="md:text-right md:w-1/2 w-full text-sm">©2024 TENZRO</p>
         </div>
-      </div>
-    </div>
+      </body>
+    </html>
   )
 }
