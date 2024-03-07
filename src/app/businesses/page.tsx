9d3c6d66398e8570b63776212fcf3dd5814bd10f
Hilal Agil <hilaal@gmail.com>
2024-03-07 18:43:17 +0100

    add usecases

--- src/app/businesses/page.tsx
+++ src/app/businesses/page.tsx
@@ -11,6 +11,80 @@ export default function Businesses() {
                     </div>
                 </div>
             </div>
+            <div className="container mx-auto">
+                <div className="md:flex flex-wrap">
+                    <div className="md:w-1/4 px-6 md:px-2 mb-6 md:mb-4">
+                        <div style={{ borderColor: '#D5E4D5' }} className="border p-4 md:min-h-96 flex rounded-xl">
+                            <div className="text-center px-2 py-4">
+                                <h2 className="mono-medium-h text-xl">Art</h2>
+                                <p className="mt-4 mono-light text-sm">NFTs created on Tenzro contain metadata enriched with AI-generated insights, such as artistic style analysis, provenance, and copyright information.</p>
+                                <p className="mt-4 mono-light text-sm">Through AI-driven recommendations and personalized insights, collectors can explore artworks aligned with their tastes and preferences, fostering trust and transparency in the art market.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="md:w-1/4 px-6 md:px-2 mb-6 md:mb-4">
+                        <div style={{ borderColor: '#978AAD' }} className="border p-4 md:min-h-96 flex rounded-xl">
+                            <div className="text-center px-2 py-4">
+                                <h2 className="mono-medium-h text-xl">Gaming</h2>
+                                <p className="mt-4 mono-light text-sm">Tenzro NFTs can contain metadata that encode game state information, enabling AI-driven procedural generation and content customization.</p>
+                                <p className="mt-4 mono-light text-sm">Game developers can leverage Tenzro&apos;s AI-infused NFTs to design personalized player engagement strategies.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="md:w-1/4 px-6 md:px-2 mb-6 md:mb-4">
+                        <div style={{ borderColor: '#FF6A3B' }} className="border p-4 md:min-h-96 flex rounded-xl">
+                            <div className="text-center px-2 py-4">
+                                <h2 className="mono-medium-h text-xl">Fashion</h2>
+                                <p className="mt-4 mono-light text-sm">Tenzro NFTs contain vector embeddings that can encode garment attributes, such as fabric texture, color, and fit, enhancing realism and accuracy in virtual styling.</p>
+                                <p className="mt-4 mono-light text-sm">Fashion retailers can use Tenzro&apos;s AI-infused NFTs to offer virtual styling consultations where users receive personalized fashion advice and outfit recommendations.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="md:w-1/4 px-6 md:px-2 mb-6 md:mb-4">
+                        <div style={{ borderColor: '#E4D353' }} className="border p-4 md:min-h-96 flex rounded-xl">
+                            <div className="text-center px-2 py-4">
+                                <h2 className="mono-medium-h text-xl">Healthcare</h2>
+                                <p className="mt-4 mono-light text-sm">Combining wearable data with Tenzro&apos;s technology, individuals can have their health profiles encoded into NFTs with vector embeddings, enabling personalized continuous health tracking.</p>
+                                <p className="mt-4 mono-light text-sm">Furthermore, tokenized incentivized programs can reward healthy behaviors, fostering a proactive approach to wellness management.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="md:w-1/4 px-6 md:px-2 mb-6 md:mb-4">
+                        <div style={{ borderColor: '#B3D238' }} className="border p-4 md:min-h-96 flex rounded-xl">
+                            <div className="text-center px-2 py-4">
+                                <h2 className="mono-medium-h text-xl">Entertainment</h2>
+                                <p className="mt-4 mono-light text-sm">Each token contains vector embeddings that capture user preferences, behavior patterns, and content affinities, enabling dynamic content curation and recommendation.</p>
+                                <p className="mt-4 mono-light text-sm">Entertainment platforms can leverage Tenzro&apos;s AI-powered tokens to offer hyper-personalized streaming experiences where users receive tailored recommendations based on their unique tastes and preferences.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="md:w-1/4 px-6 md:px-2 mb-6 md:mb-4">
+                        <div style={{ borderColor: '#7D8F70' }} className="border p-4 md:min-h-96 flex rounded-xl">
+                            <div className="text-center px-2 py-4">
+                                <h2 className="mono-medium-h text-xl">Finance</h2>
+                                <p className="mt-4 mono-light text-sm">Tenzro&apos;s AI can provide personalized investment recommendations, empowering users to make informed financial decisions on digital assets.</p>
+                                <p className="mt-4 mono-light text-sm">DeFi platforms can leverage Tenzro’s AI to create social trading networks where users can follow and replicate the investment strategies of top-performing traders.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="md:w-1/4 px-6 md:px-2 mb-6 md:mb-4">
+                        <div style={{ borderColor: '#9DC2C0' }} className="border p-4 md:min-h-96 flex rounded-xl">
+                            <div className="text-center px-2 py-4">
+                                <h2 className="mono-medium-h text-xl">E-commerce</h2>
+                                <p className="mt-4 mono-light text-sm">Tenzro&apos;s AI can provide dApps the capability to offer personalized shopping experiences, recommending products, promotions, and discounts tailored to individual preferences and behavior.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="md:w-1/4 px-6 md:px-2 mb-6 md:mb-4">
+                        <div style={{ borderColor: '#668FA9' }} className="border p-4 md:min-h-96 flex rounded-xl">
+                            <div className="text-center px-2 py-4">
+                                <h2 className="mono-medium-h text-xl">Real Estate</h2>
+                                <p className="mt-4 mono-light text-sm">Real Estate can be tokenized through NFTs on Tenzro, whith the added AI features that can analyze property data, market trends, and historical sales to accurately assess property values and predict future trends.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
