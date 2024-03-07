eb0dd2a9c50d13b282905b1830d73c23903006ec
Hilal Agil <hilaal@gmail.com>
2024-03-07 18:00:17 +0100

    Update index.jsx

--- src/components/header/nav/Body/index.jsx
+++ src/components/header/nav/Body/index.jsx
@@ -1,45 +1,62 @@
 // nav/Body/index.jsx
 
-import { motion } from 'framer-motion';
-import Link from 'next/link';
-import styles from './style.module.scss';
-import { blur, translate } from '../../anim';
+import { motion } from "framer-motion";
+import Link from "next/link";
+import styles from "./style.module.scss";
+import { blur, translate } from "../../anim";
+import { useRouter } from 'next/navigation';
 
-export default function Body({links, selectedLink, setSelectedLink}) {
+export default function Body({ links, selectedLink, setSelectedLink }) {
+  const router = useRouter();
 
-    const getChars = (word) => {
-        let chars = [];
-        word.split("").forEach( (char, i) => {
-          chars.push(
-            <motion.span 
-                custom={[i * 0.02, (word.length - i) * 0.01]} 
-                variants={translate} initial="initial" 
-                animate="enter" 
-                exit="exit" 
-                key={char + i}>
-                {char}
-            </motion.span>
-            )
-        })
-        return chars;
-    }
-    
-    return (
-        <div className={styles.body}>
-        {
-            links.map( (link, index) => {
-                const { title, href } = link;
-                return <Link target="_blank" key={`l_${index}`} href={href}>
-                <motion.p 
-                    onMouseOver={() => {setSelectedLink({isActive: true, index})}} 
-                    onMouseLeave={() => {setSelectedLink({isActive: false, index})}} 
-                    variants={blur} 
-                    animate={selectedLink.isActive && selectedLink.index != index ? "open" : "closed"}>
-                    {getChars(title)}
-                </motion.p>
-                </Link>
-            })
-        }
-        </div>
-    )
+  const handleClick = () => {
+    router.reload();
+  };
+
+  const getChars = (word) => {
+    let chars = [];
+    word.split("").forEach((char, i) => {
+      chars.push(
+        <motion.span
+          custom={[i * 0.02, (word.length - i) * 0.01]}
+          variants={translate}
+          initial="initial"
+          animate="enter"
+          exit="exit"
+          key={char + i}
+        >
+          {char}
+        </motion.span>
+      );
+    });
+    return chars;
+  };
+
+  return (
+    <div className={styles.body}>
+      {links.map((link, index) => {
+        const { title, href } = link;
+        return (
+          <Link onClick={handleClick} key={`l_${index}`} href={href}>
+            <motion.p
+              onMouseOver={() => {
+                setSelectedLink({ isActive: true, index });
+              }}
+              onMouseLeave={() => {
+                setSelectedLink({ isActive: false, index });
+              }}
+              variants={blur}
+              animate={
+                selectedLink.isActive && selectedLink.index != index
+                  ? "open"
+                  : "closed"
+              }
+            >
+              {getChars(title)}
+            </motion.p>
+          </Link>
+        );
+      })}
+    </div>
+  );
 }
